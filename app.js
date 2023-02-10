3293662c070602cfe0b8252512f115fce114967f
iLwSZ <black.alex2010@yahoo.com>
2023-02-10 12:21:52 +0200

    Const declaration

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const employeeList = document.getElementById("employeeList");
 const addEmployeeBtn = document.getElementById("addEmployee");
+const updateEmployeeBtn = document.getElementById("updateEmployee");
+const nameInput = document.getElementById("name");
+const ageInput = document.getElementById("age");
+const locationInput = document.getElementById("location");
+const professionInput = document.getElementById("profession");
 
 let employees = [];
